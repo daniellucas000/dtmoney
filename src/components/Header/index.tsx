06dfe184955414b09a2d617d64c4df495d0dab13
Daniel Lucas <danielluccas000@gmail.com>
2022-03-02 15:52:01 -0300

    corrigindo importações Header/TransactionModal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-import Modal from 'react-modal';
 import { Container, Content } from './styles';
 
 interface HeaderProps {
--- src/components/newTransactionModal/index.tsx
+++ src/components/newTransactionModal/index.tsx
@@ -1,7 +1,6 @@
 import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 import { useTransactions } from '../../hooks/useTransactions';
-import { api } from '../../services/api';
 import {
   Container,
   TransectionTypeContainer,
